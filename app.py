04a812d0204105e284f10c4072a6a37de54755b9
uday2319 <udaydk587@gmail.com>
2025-12-14 17:16:19 +0530

    added gunicorn for deployment

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask,jsonify
 app=Flask(__name__)
 @app.route("/")
 def home():
-    return jsonify(message="Flask CI/C working!")
+    return jsonify(message="Flask CI/CD working!")
 if __name__=="__main__":
     app.run(debug=True)
     

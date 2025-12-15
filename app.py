8698c5c979eceba3b8359cafdf606b1bffca0f55
uday2319 <udaydk587@gmail.com>
2025-12-15 12:41:04 +0530

    cicd through Docker

--- app.py
+++ app.py
@@ -2,7 +2,9 @@ from flask import Flask,jsonify
 app=Flask(__name__)
 @app.route("/")
 def home():
-    return jsonify(message="Heeey there CI/CD working through github actions!")
+    return jsonify(message="Hey! there CI/CD working through github actions!")
 if __name__=="__main__":
     app.run(debug=True)
+          
+
     
